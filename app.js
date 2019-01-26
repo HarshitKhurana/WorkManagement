129819a1ef63a520a4d8eeffde4a77aecbbf2354
HarshitKhurana <hkhurana3@gmail.com>
2019-01-26 13:14:01 +0530

    tgesting...

--- app.js
+++ app.js
@@ -4,15 +4,19 @@ var app = express();
 
 // Setting up view engine : using embedded JS
 app.set('view engine' , "ejs");
+
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-var work = [ "H1" , "T1" , "G1" , "D1"]
+var work = [ "H1" , "T1-Demo" , "G1-Demo" , "D1-Demo"]
 //var headingArr = [work[0]]
 var headingArr = ["H1" , "H2" , "H3"]
 var workList = [work]
 workList.push([ "H2" , "T2" , "G2" , "D2"])
 workList.push([ "H3" , "T3" , "G3" , "D3"])
 
+var groupList = ["G1"]
+groupList.push("G2")
 
 //     Routes definition
 
@@ -55,6 +59,49 @@ app.post('/newtodo' , function (req , res){
         res.redirect ("/"); // Send to default route which will have updated list.
   }
   
+});
+
+// Add new group
+app.post('/newGroup' , function (req , res){
+  console.log("Request : add newGroup : " , req.body);
+  var newGroupReq = req.body.group;
+  if (groupList.includes(newGroupReq))  {
+    console.error("Request failed, group already present");
+    res.redirect ("/"); // Send to default route which will have updated list.
+    return;
+  }
+  groupList.push(newGroupReq);
+  console.log ("Request : Added group");
+  res.redirect ("/"); // Send to default route which will have updated list.
+});
+
+// delete request to delete a specific task depending on heading
+app.delete("/todo/delete", function (req , res){
+  var heading = req.body.heading;     // headingis the name of input fields in index.ejs
+  console.log("Deleting  heading:",heading);
+  console.log("Current headingArr : " , headingArr)
+  console.log("Current Worklist : " , workList,"\n")
+
+  if (! headingArr.includes(heading)  )   {
+    console.log("heading not found...redirecting");
+    res.redirect ("/"); // Send to default route which will have updated list.
+    return;
+  }
+
+  try{
+     headingArr.pop(heading);
+  }
+  catch(error)  {
+    console.error("Error : ",error);
+  }
+  for (var i = 0 ; i < workList.length; i ++) {
+    if (workList[i][0] === "heading") {
+      console.log("heading found...deleting..")
+      workList.pop(i);
+    }
+  }
+  console.log("workList after deleting : " , workList);
+  res.redirect ("/"); // Send to default route which will have updated list.
 });
 
 app.listen(8080 , function (req , res) {
