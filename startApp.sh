4da7b2d0091a343cbddf04c3e0143fa4ee0cd39f
HarshitKhurana <hkhurana3@gmail.com>
2019-01-30 01:41:39 +0530

    removed creating docker image

--- startApp.sh
+++ startApp.sh
@@ -7,23 +7,22 @@ then
   exit 1
 fi
 
-echo -e "[*] Checking for presence WorkManagement image"
-docker images | grep -i workmgmt_node  | grep -i latest > /dev/null 2>&1
+echo -e "[*] Checking for presence node docker image"
+docker images | grep -i node  | grep -i latest > /dev/null 2>&1
 if [ $? -ne 0 ]
 then
-  echo -e "[#] docker images for workmgmt_node not found, generating image first"
-  docker build -t workmgmt_node -f DockerfileNode .
+  echo -e "[#] docker images for node not found, pulling image first"
+  docker pull node
   if [ $? -eq 0 ]
   then
-    echo -e "[*] workmgmt_node image build successful"
+    echo -e "[*] docker image for node pull successfully"
   else
-    echo -e "[*] workmgmt_node image build failed, exiting..."
+    echo -e "[*] docker pull failed"
     exit 1
   fi
 else
-  echo -e "[*] docker image for workmgmt_node found"
+  echo -e "[*] docker image for node found"
 fi
-
 
 echo -e "[*] Checking for presence redis image"
 docker images | grep -i redis | grep -i latest > /dev/null 2>&1
@@ -50,7 +49,8 @@ fi
 
 echo -en "[*] Starting node container for WebApp\n"
 docker rm --force work_management_webapp > /dev/null 2>&1
-docker run -dit --net=host -p 8080:8080 --name work_management_webapp -v $PWD:/app -v $PWD/views:/views  workmgmt_node node /app/app.js > /dev/null 2>&1
+npm i &> /dev/null
+docker run -it --net=host -p 8080:8080 --name work_management_webapp -v $PWD:/app -v $PWD/views:/views  workmgmt_node node /app/app.js 
 if [ $? -eq 0 ]
 then
   echo -en "[*] Server started, for accessing WebApp visit : 127.0.0.1:8080\n"
